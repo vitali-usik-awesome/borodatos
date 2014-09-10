5537014ed0702f9d73061305ae69169e7b3b3ffe
kiryll.yermakou <kiryll_yermakou@epam.com>
2014-09-10 15:02:17 +0300

    added service name

--- src/main/java/com/borodatos/service/ArticleServiceImpl.java
+++ src/main/java/com/borodatos/service/ArticleServiceImpl.java
@@ -11,17 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 import com.borodatos.dao.ArticleDAO;
 import com.borodatos.model.Article;
 
-@Service
+@Service("articleService")
 public class ArticleServiceImpl implements ArticleService {
 
 	private static Log log = LogFactory.getLog(ArticleServiceImpl.class);
-	
+
 	@Autowired
 	private ArticleDAO articleDAO;
-	
+
 	@Transactional
 	public List<Article> listArticle() {
-		
+
 		log.info("from service");
 		return articleDAO.listArticle();
 	}
